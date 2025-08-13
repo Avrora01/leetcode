5570cbb8ab733b5b064c95284b9f37362eb9d62b
Sabrina <suhrobzodasabrina01@gmail.com>
2025-08-13 09:55:38 +0500

    add lengthOfLastWord function #2

--- easy/index.js
+++ easy/index.js
@@ -2,6 +2,7 @@
 // Input: x = 121
 // Output: true
 // Explanation: 121 reads as 121 from left to right and from right to left.
+
 function isPalindrome(x) {
   let str = x.toString();
   let res = "";
@@ -10,4 +11,16 @@ function isPalindrome(x) {
   }
   return res === str;
 }
-console.log(isPalindrome(121));
+// console.log(isPalindrome(121));
+
+// 2) Given a string s consisting of words and spaces, return the length of the last word in the string.
+// A word is a maximal substring consisting of non-space characters only.
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+
+function lengthOfLastWord(s) {
+  let arr = s.trim().split(" ");
+  return arr[arr.length - 1].length;
+}
+// console.log(lengthOfLastWord("   fly me   to   the moon  "));
