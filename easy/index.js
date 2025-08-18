fee247eb26126f4404b123c09dddd128a4d1ee14
Sabrina <suhrobzodasabrina01@gmail.com>
2025-08-18 10:33:35 +0500

    add twoSum

--- easy/index.js
+++ easy/index.js
@@ -43,3 +43,15 @@ function repeatedCharacter(s) {
   return result;
 }
 // console.log(repeatedCharacter("abccbaacz"));
+
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+  return []
+}
+console.log(twoSum([2, 7, 11, 15], 9));
