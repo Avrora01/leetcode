8639416dac303f652388367d34c297dbf39e6389
Sabrina <suhrobzodasabrina01@gmail.com>
2025-08-18 10:35:04 +0500

    add

--- easy/index.js
+++ easy/index.js
@@ -25,6 +25,7 @@ function lengthOfLastWord(s) {
 }
 // console.log(lengthOfLastWord("   fly me   to   the moon  "));
 
+// 3)
 function repeatedCharacter(s) {
   let arr = s.split('');
   let firstDuplicateIndex = s.length;
@@ -44,6 +45,14 @@ function repeatedCharacter(s) {
 }
 // console.log(repeatedCharacter("abccbaacz"));
 
+// 4)
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 function twoSum(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
