ff5f2bda626fb790a4371f185bb270d238ccc112
Sabrina <suhrobzodasabrina01@gmail.com>
2025-08-19 09:55:17 +0500

    romanToInt

--- easy/index.js
+++ easy/index.js
@@ -82,19 +82,76 @@ function twoSum(nums, target) {
 // Thus, the result should be [1,2,4].
 
 
-function plusOne(digits){
+function plusOne(digits) {
 
-    for(let i = digits.length - 1; i >= 0; i--) {
-        if(digits[i] === 9) {
-            digits[i] = 0
-        }
-        else {
-            digits[i] += 1
-            return digits
-        }
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] === 9) {
+      digits[i] = 0
     }
+    else {
+      digits[i] += 1
+      return digits
+    }
+  }
 
-    return [1, ...digits]
+  return [1, ...digits]
 }
 // console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]));
 
+
+// 6)
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, 
+// which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+// Roman numerals are usually written largest to smallest from left to right. 
+// However, the numeral for four is not IIII. Instead, the number four is written as IV. 
+// Because the one is before the five we subtract it making four. The same principle applies to the 
+// number nine, which is written as IX. There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+ 
+
+// Example 1:
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+
+function romanToInt(s) {
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  }
+  let res = 0;
+
+  for(let i=0; i<s.length; i++){
+      const current = values[s[i]]
+      const next = values[s[i+1]]
+
+      if(next>current){
+         res-=current
+      }
+      else{
+         res+=current
+      }
+  }
+  return res
+
+}
+// console.log(romanToInt("III"));
