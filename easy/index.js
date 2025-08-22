01342f5742c13f3d04377fbe005064a108c520da
Sabrina <suhrobzodasabrina01@gmail.com>
2025-08-22 14:55:45 +0500

    add toLowerCase

--- easy/index.js
+++ easy/index.js
@@ -122,3 +122,17 @@ function addDigits(num) {
 }
 
 // console.log(addDigits(38));
+
+
+//7) Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+// Example 1:
+// Input: s = "Hello"
+// Output: "hello"
+
+
+function toLowerCase(s) {
+  arr = s.toLowerCase()
+  return arr;
+}
+
+console.log(toLowerCase("Hello"));
