631adc35cc8b5877bbe43c1120b358d525409452
Sabrina <suhrobzodasabrina01@gmail.com>
2025-09-17 10:54:21 +0500

    add earliestTime

--- easy/index.js
+++ easy/index.js
@@ -158,5 +158,27 @@ function checkIfPangram(sentence) {
 
 // console.log(checkIfPangram("leetcode"));
 
+//9) Earliest Time to Finish One Task
 
+// You are given a 2D integer array tasks where tasks[i] = [si, ti].
+// Each [si, ti] in tasks represents a task with start time si that takes ti units of time to finish.
+// Return the earliest time at which at least one task is finished.
+
+// Example 1:
+// Input: tasks = [[1,6],[2,3]]
+// Output: 5
+// Explanation:
+// The first task starts at time t = 1 and finishes at time 1 + 6 = 7. The second task finishes at time 2 + 3 = 5. You can finish one task at time 5.
+
+function earliestTime(tasks) {
+  let minEl = Infinity
+  for (let i = 0; i < tasks.length; i++) {
+    let sum = tasks[i][0] + tasks[i][1]
+    if (sum < minEl) {
+      minEl = sum
+    }
+  }
+  return minEl
+}
+// console.log(earliestTime([[1, 6], [2, 3]]));
 
