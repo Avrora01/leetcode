d495598ddd0e18e7aacd7fa771e77c8dbfbedd43
Sabrina <suhrobzodasabrina01@gmail.com>
2025-10-03 11:43:51 +0500

    add Reverse Integer

--- midlle/index.js
+++ midlle/index.js
@@ -109,7 +109,7 @@ function maxBottlesDrunk(numBottles, numExchange, empty = 0) {
   empty += numBottles
   if (empty < numExchange) {
     return numBottles
-  } 
+  }
   let newBottle = 1;
   empty -= numExchange;
   numExchange += 1;
@@ -118,3 +118,33 @@ function maxBottlesDrunk(numBottles, numExchange, empty = 0) {
 }
 
 // console.log(maxBottlesDrunk(13, 6))
+
+//4) Reverse Integer
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+// Example 1:
+// Input: x = 123
+// Output: 321
+
+// Example 2:
+// Input: x = -123
+// Output: -321
+
+// Example 3:
+// Input: x = 120
+// Output: 21
+ 
+// Constraints:
+// -231 <= x <= 231 - 1
+
+function reverse(x) {
+  let reversed = 0
+  while (x !== 0) {
+    let digit = x % 10;
+    reversed = reversed * 10 + digit
+    x = Math.trunc(x / 10)
+  }
+  return reversed
+}
+console.log(reverse(-123));
