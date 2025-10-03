cf836f6e20758b5760b274c61afdffc0185aa57f
Sabrina <suhrobzodasabrina01@gmail.com>
2025-10-03 14:18:22 +0500

    Ugly Number

--- easy/index.js
+++ easy/index.js
@@ -216,7 +216,7 @@ function maxKDistinct(nums, k) {
 function containsDuplicate(nums) {
   return new Set(nums).size !== nums.length
 }
-console.log(containsDuplicate([1, 2, 3]));
+// console.log(containsDuplicate([1, 2, 3]));
 
 
 //12) Water Bottles
@@ -273,3 +273,42 @@ function mySqrt(x) {
 }
 
 // console.log(mySqrt(8));
+
+//14 Ugly Number
+// An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+// Given an integer n, return true if n is an ugly number.
+
+// Example 1:
+
+// Input: n = 6
+// Output: true
+// Explanation: 6 = 2 × 3
+
+// Example 2:
+// Input: n = 1
+// Output: true
+// Explanation: 1 has no prime factors.
+
+// Example 3:
+// Input: n = 14
+// Output: false
+// Explanation: 14 is not ugly since it includes the prime factor 7.
+
+
+function isUgly(n) {
+  if (n <= 0) return false; 
+  while (n % 2 === 0) {
+    n = n / 2;
+  }
+  while (n % 3 === 0) {
+    n = n / 3;
+  }
+  while (n % 5 === 0) {
+    n = n / 5;
+  }
+  return n === 1;
+}
+
+console.log(isUgly(6));  // true  (6 = 2×3)
+console.log(isUgly(14)); // false (14 = 2×7)
+console.log(isUgly(1));  // true  (по условию)
